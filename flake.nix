78a7a706c934629ea40c0b723d350edcdf15b05a
Teilzeitcatgirl Kesefon <kesefon@pm.me>
2026-01-12 21:00:09 +0100

    unstabeldideltu

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # /etc/nixos/flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     zen-browser.url = "github:youwen5/zen-browser-flake";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
