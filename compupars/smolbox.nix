7c9bceb330bdf7e60a10eea8289b846824397f89
Teilzeitcatgirl Kesefon <kesefon@pm.me>
2026-01-12 01:02:11 +0100

    Update smolbox.nix

--- compupars/smolbox.nix
+++ compupars/smolbox.nix
@@ -22,6 +22,13 @@ in
 
   networking.hostName = "smolbox";
 
+  boot.supportedFilesystems = lib.mkForce [
+        "vfat"
+        "fat32"
+        "exfat"
+        "ext4"
+        "btrfs"
+      ];
   boot.initrd.includeDefaultModules = lib.mkForce false;
   boot.initrd.allowMissingModules = true;
 
