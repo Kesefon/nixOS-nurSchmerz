74f7b0f35ae9d87d95a16e2d5f48c45a73a4447b
Teilzeitcatgirl Kesefon <kesefon@pm.me>
2026-01-12 02:40:58 +0100

    Update and rename desktop-base.nix to base.nix

--- common/base.nix
+++ common/base.nix
@@ -1,28 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [
-    ./zen.nix
-  ];
-
-  # Enable OpenGL/Vulkan
-  hardware.graphics = lib.mkDefault {
-    enable = true;
-    #enable32Bit = true;
-  };
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # TPM2
-  # Note: Not all my machines have a tpm module
-  #security.tpm2.enable = true;
-  #security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
-  #security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
-
-  # Use latest kernel.
-  #boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -46,35 +27,11 @@
   # Configure console keymap
   console.keyMap = lib.mkDefault "de";
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    wayland.compositor = "kwin";
-  };
-  services.desktopManager.plasma6.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kesefon = {
     isNormalUser = true;
     description = "Kesefon";
-    extraGroups = [ "networkmanager" "wheel" "tss" "systemd-journal" ];
+    extraGroups = [ "networkmanager" "wheel" "systemd-journal" ];
   };
 
   # Allow unfree packages
@@ -86,23 +43,10 @@
     usbutils
     pciutils
     file
-    #spotify
-    kdePackages.kate
     git
-    #discord
-    mpv
-    #krita
-    #onlyoffice-desktopeditors
-    #xournalpp
   ];
 
   programs.fish.enable = true;
-  #programs.steam = {
-    #localNetworkGameTransfers.openFirewall = true;
-    #enable = true;
-  #};
-
-  programs.kdeconnect.enable = true;
 
   programs.bash = {
     interactiveShellInit = ''
