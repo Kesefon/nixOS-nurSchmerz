c681ff5a591c4911219aa5ccb0a7250ce3b84a9f
kesefon <kesefon@pm.me>
2026-01-12 23:22:18 +0100

    cleanup

--- common/base.nix
+++ common/base.nix
@@ -19,12 +19,6 @@
     LC_ALL = "en_US.UTF-8";
   };
 
-  # Configure keymap in X11
-  #services.xserver.xkb = lib.mkDefault {
-   # layout = "de";
-    #variant = "";
-  #};
-
   # Configure console keymap
   console.keyMap = lib.mkDefault "de";
 
@@ -47,17 +41,17 @@
     git
   ];
 
-  #programs.fish.enable = true;
+  programs.fish.enable = true;
 
-  #programs.bash = {
-  #  interactiveShellInit = ''
-  #    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-  #    then
-  #      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-  #      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-  #    fi
-  #  '';
-  #};
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
---
+++ common/desktop-base.nix
@@ -0,0 +1,79 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./zen.nix
+    ./base.nix
+  ];
+
+  # Enable OpenGL/Vulkan
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # TPM2
+  # Note: Not all my machines have a tpm module
+  security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Configure keymap in X11
+  services.xserver.xkb = lib.mkDefault {
+    layout = "de";
+    variant = "";
+  };
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    wayland.compositor = "kwin";
+  };
+  services.desktopManager.plasma6.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kesefon = {
+    isNormalUser = true;
+    description = "Kesefon";
+    extraGroups = [ "networkmanager" "wheel" "tss" "systemd-journal" ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    spotify
+    kdePackages.kate
+    discord
+    mpv
+    krita
+    onlyoffice-desktopeditors
+    xournalpp
+  ];
+
+  programs.steam = {
+    localNetworkGameTransfers.openFirewall = true;
+    enable = true;
+  };
+
+  programs.kdeconnect.enable = true;
+}
