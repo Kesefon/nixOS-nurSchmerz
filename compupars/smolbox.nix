e658d9c11ca389111076339c30534d0c8b2a65f5
Teilzeitcatgirl Kesefon <kesefon@pm.me>
2026-01-12 02:32:32 +0100

    Update smolbox.nix

--- compupars/smolbox.nix
+++ compupars/smolbox.nix
@@ -25,10 +25,9 @@ in
   boot.initrd.includeDefaultModules = lib.mkForce false;
   boot.initrd.allowMissingModules = true;
 
-  boot.kernelPackages = pkgs.linuxPackagesFor (rockchip_kernel.overrideAttrs (old: {
-    dontStrip = true;
-    doStrip = false;
-  }));
+  boot.kernelPackages = (pkgs.linuxPackagesFor rockchip_kernel).override {
+    targets = { modules-shrunk = null; };
+  };
 
   boot.initrd.availableKernelModules = [ ];
   boot.initrd.kernelModules = [ ];
