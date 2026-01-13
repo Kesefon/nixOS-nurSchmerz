c8215a7102dafdac5167eca21d6b28a816ff1b04
kesefon <kesefon@pm.me>
2026-01-13 18:03:46 +0100

    missed that

--- common/postgresql.nix
+++ common/postgresql.nix
@@ -3,13 +3,5 @@
 {
   config.services.postgresql = {
     enable = true;
-    #ensureUsers = [{
-    #  name = "postgresql";
-    #}];
-
-    #authentication = pkgs.lib.mkOverride 10 ''
-    #  #type database  DBuser  auth-method
-    #  local sameuser  all     peer
-    #'';
   };
 }
