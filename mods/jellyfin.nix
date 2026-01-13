b75610a4d89eca057eea9c3120a8a2e6b82f2e86
kesefon <kesefon@pm.me>
2026-01-13 22:01:15 +0100

    jellyfin hwaccel

--- mods/jellyfin.nix
+++ mods/jellyfin.nix
@@ -4,6 +4,27 @@
   services.jellyfin = {
     enable = true;
     dataDir = "/mnt/data/jellyfin";
+    hardwareAcceleration = {
+      enable = true;
+      type = "rkmpp";
+      device = "/dev/dri/renderD128";
+    };
+    forceEncodingConfig = true;
+    transcoding = {
+      hardwareEncodingCodecs = {
+        hevc = true;
+        av1 = false;
+      };
+      hardwareDecodingCodecs = {
+        vp9 = true;
+        vp8 = true;
+        hevc10bit = true;
+        hevc = true;
+        h264 = true;
+        av1 = true;
+      };
+      enableHardwareEncoding = true;
+    };
   };
 
   services.nginx.virtualHosts."watch.in.froggo.garden" = {
