3c4f0de9f75acc65c29e594695dc8ca3039a728e
Kesefon <kesefon@pm.me>
2026-02-15 14:46:31 +0100

    sharkey media folder

--- mods/sharkey.nix
+++ mods/sharkey.nix
@@ -10,6 +10,18 @@
     };
   };
 
+  systemd.tmpfiles.settings = {
+    "10-sharkey-media" = {
+      "/mnt/data/sharkey/media" = {
+        d = {
+          user = "sharkey";
+          group = "sharkey";
+          mode = "660";
+        };
+      };
+    };
+  };
+
   services.nginx.virtualHosts."froggo.garden" = {
     locations."/" = {
       proxyPass = "http://localhost:3000";
