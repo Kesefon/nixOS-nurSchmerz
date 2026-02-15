66dbef5a2f86f23108286c8b6f258e3d3a91fa3b
Kesefon <kesefon@pm.me>
2026-02-15 18:09:36 +0100

    Allow wg port in firewall

--- mods/wireguard-server.nix
+++ mods/wireguard-server.nix
@@ -1,7 +1,10 @@
 { ... }:
 
 {
+  # We use port 80 to avoid strict firewalls in public wifis
   networking.wg-quick.interfaces.froggo.configFile = "/mnt/data/secrets/wireguard.conf";
+
+  networking.firewall.allowedUDPPorts = [ 80 ];
 
   services.nginx.streamConfig = "
     server {
