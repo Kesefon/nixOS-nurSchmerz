c83f4817608c7f1a2136c2d20514848c26fb2a21
Kesefon <kesefon@pm.me>
2026-02-15 18:18:41 +0100

    simplify wireguard-server.nix

--- mods/wireguard-server.nix
+++ mods/wireguard-server.nix
@@ -5,11 +5,4 @@
   networking.wg-quick.interfaces.froggo.configFile = "/mnt/data/secrets/wireguard.conf";
 
   networking.firewall.allowedUDPPorts = [ 80 ];
-
-  services.nginx.streamConfig = "
-    server {
-      listen 80 udp;
-      proxy_pass 192.168.0.1:8095;
-    }
-  ";
 }
