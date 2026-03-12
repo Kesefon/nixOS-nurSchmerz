9c2d2e3a226965ba081e497d57d665490c30d50b
Teilzeitcatgirl Kesefon <kesefon@pm.me>
2026-03-12 18:03:24 +0100

    script now worki ?

--- mods/service-fail-alert.nix
+++ mods/service-fail-alert.nix
@@ -5,12 +5,10 @@
 let
   service-fail-alert = pkgs.writeScript "service-fail-alert"
     ''
-      #!/usr/bin/env nix-shell
-      #! nix-shell -i bash --pure
-      #! nix-shell -p bash cacert curl hostname
-      curl \
+      #!/bin/sh
+      ${pkgs.curl}/bin/curl \
         -H "Authorization: Bearer $(cat ${config.age.secrets.ntfy-token.path})" \
-        -d "$1 failed on $(hostname)" \
+        -d "$1 failed on $(${pkgs.hostname}/bin/hostname)" \
         https://ntfy.froggo.garden/service-fail-alert
     '';
 in
@@ -21,7 +19,6 @@ in
   config.systemd.services."service-fail-alert@" = {
     description = "Send notification on service failure";
     onFailure = lib.mkForce [];
-    path = [ pkgs.nix ];
     serviceConfig = {
       ExecStart = "${service-fail-alert} %i";
       Type = "oneshot";
