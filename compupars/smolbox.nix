380052c48760ff09efc71e09d7472bf999bd9a35
kesefon <kesefon@pm.me>
2026-03-12 22:11:02 +0100

    limit update memory on smolbox

--- compupars/smolbox.nix
+++ compupars/smolbox.nix
@@ -25,6 +25,9 @@
       upper = "08:00";
     };
   };
+  # Some updates require compiling huge programs which is a horrible idea on this teeny tiny SBC
+  # Just kill the update if that happens
+  systemd.services."nixos-upgrade".serviceConfig.MemoryMax = "5G";
 
   boot = {
     kernelPackages = pkgs.linuxPackages_testing;
