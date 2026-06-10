e25ab78db287622f0be58bd67336facc56a8377e
kesefon <kesefon@pm.me>
2026-06-10 13:56:45 +0200

    update mloader

--- mods/mloader-fork.nix
+++ mods/mloader-fork.nix
@@ -12,8 +12,8 @@ python3Packages.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "kesefon";
     repo = "mloader";
-    rev = "0e1d7e5301aee16581a4d498f552ede36684fc95";
-    hash = "sha256-txVIFDqy3UJPXYnYzWhnC0Aejn4FeX4Eed+W//E12aI=";
+    rev = "9be124ddfa513ed055356bc92295a2f403bfbd1e";
+    hash = "sha256-Jeg4hbg+2y6Lub/D84as1EhPxU8PBj//X3zRHMkN0m0=";
   };
 
   build-system = with python3Packages; [ setuptools ];
