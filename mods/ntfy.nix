65eabc4991de461be0d307fb6b9b7f6eb3dc76e4
kesefon <kesefon@pm.me>
2026-09-23 00:46:51 +0200

    fix ntfy perms

--- mods/ntfy.nix
+++ mods/ntfy.nix
@@ -7,25 +7,9 @@
       base-url = "https://ntfy.froggo.garden/";
       listen-http = ":3001";
       behind-proxy = true;
-      auth-file = "/mnt/data/ntfy/user.db";
       auth-default-access = "deny-all";
       auth-access = [ "*:up*:wo" ];
       enable-login = true;
-      attachment-cache-dir = "/mnt/data/ntfy/attachmments";
-    };
-  };
-
-  systemd.services.ntfy-sh.serviceConfig.ReadWritePaths = [ "/mnt/data/ntfy/" ];
-
-  systemd.tmpfiles.settings = {
-    "10-ntfy-data" = {
-      "/mnt/data/ntfy" = {
-        d = {
-          user = "ntfy-sh";
-          group = "ntfy-sh";
-          mode = "770";
-        };
-      };
     };
   };
 
